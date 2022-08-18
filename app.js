d271478d644fb7bfdefa54a972292e34f5b71f7e
Koose <KooseTheBlogger@gmail.com>
2022-08-18 18:33:20 +0000

    post man tested

--- app.js
+++ app.js
@@ -1,8 +1,34 @@
 const express = require("express")
-
+const bodyParser = require("body-parser")
+const cors = require("cors")
+const helmet = require("helmet")
+const morgan = require("morgan")
 const app = express()
 
+//API SECURITY
+app.use(helmet())
+
+//handle cors Error
+app.use(cors())
+
+//Logger 
+app.use(morgan("tiny"))
+
+//Set Body-parser
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json())
+
+
 const port = process.env.PORT || 4001
+
+app.use("/", (req, res) => {
+    res.json({
+        message: "Breakfast Koose"
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`ApI is ready on http://localhost:${port}`)
